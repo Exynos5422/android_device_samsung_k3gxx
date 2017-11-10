81d6d4e8ab1ebaa5e78661ee1ccfa2b6884bbfc5
somadsul <somnathadsul@gmail.com>
2017-11-10 09:42:22 +0000

    K3gxx: correction of build.prop

--- device.mk
+++ device.mk
@@ -229,7 +229,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik/Art heap settings
 PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.heapstartsize=8m \
-	dalvik.vm.heapgrowthlimit=256m \
+	dalvik.vm.heapgrowthlimit=128m \
 	dalvik.vm.heapsize=512m \
 	dalvik.vm.heaptargetutilization=0.75 \
 	dalvik.vm.heapminfree=2m \
@@ -240,13 +240,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hwui.texture_cache_size=50 \
 	ro.hwui.layer_cache_size=34 \
 	ro.hwui.path_cache_size=10 \
+	ro.hwui.texture_cache_flushrate=0.4 \
 	ro.hwui.shape_cache_size=4 \
 	ro.hwui.gradient_cache_size=2 \
 	ro.hwui.drop_shadow_cache_size=6 \
-	ro.hwui.text_small_cache_width=2048 \
-	ro.hwui.text_small_cache_height=2048 \
-	ro.hwui.text_large_cache_width=4096 \
-	ro.hwui.text_large_cache_height=4096
+	ro.hwui.r_buffer_cache_size=4 \
+	ro.hwui.text_small_cache_width=1024 \
+	ro.hwui.text_small_cache_height=1024 \
+	ro.hwui.text_large_cache_width=2048 \
+	ro.hwui.text_large_cache_height=1024 \
+	ro.hwui.fbo_cache_size=16
 
 # call dalvik heap and hwui config
 # $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
